5888b31fdbed5753292dc5970d278766a0f7c684
hyungus06 <hyungus723@gmail.com>
2026-09-15 05:37:12 +0900

    [chore] Spring Boot 起動処理にコメント追加

--- src/main/kotlin/com/donghyun/simpleapi/SimpleapiApplication.kt
+++ src/main/kotlin/com/donghyun/simpleapi/SimpleapiApplication.kt
@@ -1,11 +1,19 @@
+// このファイルが属するパッケージを指定する
 package com.donghyun.simpleapi
 
+// Spring Bootの自動設定を有効にするためのアノテーション
 import org.springframework.boot.autoconfigure.SpringBootApplication
+// Spring Bootアプリケーションを起動するための関数
 import org.springframework.boot.runApplication
 
+
+// 自動設定やコンポーネントスキャンを有効にする
 @SpringBootApplication
+// Spring Bootアプリケーションの起点となるクラス
 class SimpleapiApplication
 
+// アプリケーションのエントリーポイント
 fun main(args: Array<String>) {
+    // SimpleapiApplicationを起点としてSpring Bootを起動する
 	runApplication<SimpleapiApplication>(*args)
 }
